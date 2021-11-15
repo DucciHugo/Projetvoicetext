183df3b5382917d6269165efede37a82b91a3733
Alexis <alexisvedrenne482@gmail.com>
2021-11-15 15:07:59 +0100

    Création du server js

--- js/server.js
+++ js/server.js
@@ -5,14 +5,15 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
- 
+
+
 io.on('connection', function(socket){
-  socket.on('chatMessage', function(msg){
-   io.emit('chatMessage', msg);
+  socket.on('up', function(msg){
+   io.emit('up', msg);
   });
 });
  
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080; 
  
 http.listen(port, function(){
   console.log('Serveur ouvert sur le port :' + port);
