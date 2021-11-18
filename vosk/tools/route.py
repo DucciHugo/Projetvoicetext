c713b3bef89ad366f2cdedcbea4223c4406b253d
raspi <raspi@pi.com>
2021-11-18 19:28:49 +0000

    Finsalisation des transcriptions de l'audio

--- vosk/tools/route.py
+++ vosk/tools/route.py
@@ -2,16 +2,24 @@ from tools import tools as t
 
 class Route():
 
-    mois=""
-    annee=""
-    jour=""
+    mois=" "
+    nb=" "
+    jour=" "
+    dest=" "
+    ville=" "
+    adulte=""
+    enfant=""
+    bebe=""
     def __init__(self) -> None:
        self.jour=" "
-       self.annee=" "
+       self.nb= " "
        self.jour=" "
+       self.enfant="0"
+       self.bebe="0"
+       self.adulte="0"
     def traitMois(self,mois,chiffre,text):
         for i in mois:
-            if i in text:
+            if i.lower() in text:
                 self.mois=i
         for i in chiffre:
             if i in text:
@@ -21,6 +29,31 @@ class Route():
            return True
         return False
 
+    def traitDest(self,dest,text):
+        for i in dest:
+           if i in text:
+               self.dest=i
+               return True
+   
+    def traitNb(self,lettre,text):
+         for i in lettre:
+            if i in text:
+                self.nb=i
+    def traitVille(self,ville,text):
+        for i in ville:
+            if i.lower() in text:
+                  self.ville=i
+                  return True
+        return False
+    def traitDevis(self,lettre,text):
+          for i in lettre:
+             print(i)
+             if ((i in text)and("adulte" in text)and(self.adulte=="0")):
+                  self.adulte=i
+             elif ((i in text)and("enfant" in text)and(self.enfant=="0")and(self.adulte!="0")):
+                  self.enfant=i
+             elif ((i in text)and("bébé" in text)and(self.bebe=="0")and(self.adulte!="0")and(self.enfant!="0")):
+                  self.bebe=i
     def transcription(self,text):
         if "complète" in text:
             return "J'ai compris que vous voulez une ,pension-complète"
@@ -46,8 +79,30 @@ class Route():
             return "J'ai compris que vous voulez une ,contre-proposition"
         elif "proposition" in text:
             return "J'ai compris que vous voulez une ,contre-proposition"
-        elif (True if self.traitMois(t.mois,t.lettre,text)==True else False):
-            return "J'ai compris que vous voulez partir en ,"+self.jour+" "+self.mois
+        elif (True if self.traitMois(t.mois,t.lettre,text)==True else False) and ("adulte" not in text) or ("enfant" not in text) or ("bébé" not in text):
+             return "J'ai compris que vous voulez partir le ,"+self.jour+" "+self.mois
+        elif (True if self.traitDest(t.destination,text)==True else False):
+            if self.dest=="séchelles":
+                  self.dest="seychelles"
+            return "J'ai compris que vous voulez faire un devis pour ,"+self.dest
+        elif "nuit" in text:
+             self.traitNb(t.lettre,text)
+             return "Vous avez mis ,"+self.nb+" nuit"
+        elif (True if self.traitVille(t.ville,text)==True else False):
+             return "J'ai compris que vous partez de ,"+self.ville
+        elif "adulte" in text  and "enfant" in text and "bébé" in text:
+              self.traitDevis(t.lettreDes,text)
+              return "J'ai compris que vous voulez un devis pour ,"+self.adulte+" adulte "+self.enfant+" enfant "+self.bebe+" bébé"
+        elif "adulte" in text and "enfant" in text:
+             return  "j'ai compris que vous voulez un devis pour ,adulte enfant"
+        elif "adulte" in text and "bébé" in text:
+              return "J'ai compris que vous voulez un devis pour ,adulte bébé"
+        elif "enfant" in text:
+              return "J'ai compris que vous voulez un devis pour ,enfant"
+        elif "bébé" in text:
+              return "J'ai compris que vous voulez un devis pour ,bébé"
+        elif "adulte" in text:
+              return "J'ai compris que vous voulez un devis pour ,adulte"
         else:
             return "Je n'ai pas compris votre demande de : ,"+text
 
--- vosk/tools/tools.py
+++ vosk/tools/tools.py
@@ -5,3 +5,51 @@ chiffre=['0','1','2','3','4','5','6','7','8','9']
 
 mois=['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre']
 
+destination=['martinique','guadeloupe', 'réunion', 'séchelles','maurice','maldives','dubaï','tahiti']
+
+lettreDes=['un','deux','trois','quatre','cinq','six','sept','huit','neuf','dix','onze','douze','treize'
+,'quatorze','quinze','seize','vingt','trente','quarante','cinquante','soixante','cent','mille']
+
+ville=['Paris','Marseille','Lyon',  'Toulouse', 'Nice','Nantes','Montpellier', 
+'Strasbourg',
+'Bordeaux',
+'Lille', 
+'Rennes', 
+'Reims',
+'toulon', 
+'Saint-Etienne', 
+'Le Havre',
+'Grenoble', 
+'Dijon',
+'Angers', 
+'Villeurbanne', 
+'Saint-Denis',
+'Nîmes',
+'Clermont-Ferrand',
+'Le Mans', 
+'Aix-en-Provence',
+'Brest',
+'Tours', 
+'Amiens',
+'Limoges',
+'Annecy',
+'Boulogne-Billancourt',
+'Perpignan', 
+'Besançon', 
+'Metz', 
+'Orléans', 
+'Saint-Denis', 
+'Rouen', 
+'Argenteuil',
+'Mulhouse', 
+'Caen', 
+'Nancy', 
+'Saint-Paul',
+'Roubaix', 
+'Nanterre', 
+'Vitry-sur-Seine', 
+'Créteil',
+'Avignon',
+'Poitiers',
+'Aubervilliers',
+]
